1e3f3ae71309529ca71e26173a45b5c558f303b3
Sergii Lischuk <sergiy.lischuk@clarity-ag.net>
2019-05-01 15:51:02 +0300

    Base template

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Snap");
+import html2canvas from 'html2canvas'
+import "./style.sass"
+
+html2canvas(document.querySelector("#capture")).then(canvas => {
+    document.body.appendChild(canvas)
+    
+});
